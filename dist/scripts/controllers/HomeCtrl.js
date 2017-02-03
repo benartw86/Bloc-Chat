5e6bb9cb2f8629d21acd75037e832ad22c03c947
Ben White <masinfa@gmail.com>
2017-02-03 06:55:56 -0500

    Modal template added and changes to HomeCtrl, issues with errors in HomeCtrl

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,25 @@
 (function() {
-    function HomeCtrl(Room) {  //inject the room service in order to assign the array of objects retrieved by "all" method
+    function HomeCtrl(Room, $uibModal, $uibModalInstance) {  //inject the room service in order to assign the array of objects retrieved by "all" method
         this.rooms = Room.all; 
-    }
-    
+        
+        this.open = function() {
+        var modalInstance = $uibModal.open({  //uib modal creates a object on to which you can add functionality.  Imagine the uib modal as the container that opens up when you click the modal button
+            ariaLabelledBy: 'modal-header',
+            templateUrl: 'templates/modal.html',
+            controller: 'HomeCtrl',
+            controllerAs: '$ctrl'
+        });
+     
+    }; 
+        
+        this.close = function() {
+            $uibModalInstance.dismiss('cancel');   
+    };
+
+  }
+
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', '$uibModal', '$uibModalInstance', HomeCtrl]);  //do we need ui.boostrap here
 })();
+
