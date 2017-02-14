79a19c3092e5022ac1ef1a768b0bb3715b88523a
Ben White <masinfa@gmail.com>
2017-02-13 20:45:28 -0500

    List messages complete, chat rooms clicked reveal convos

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,21 +1,28 @@
 (function() {
-    function HomeCtrl(Room, $uibModal) {  //inject the room service in order to assign the array of objects retrieved by "all" method
+    function HomeCtrl(Room, $uibModal, Message) {  //inject the room service in order to assign the array of objects retrieved by "all" method
         this.rooms = Room.all; 
+        this.room = null;
+        this.messages = null;
         
-        this.open = function() {
-        var modalInstance = $uibModal.open({  //uib modal creates a object on to which you can add functionality.  Imagine the uib modal as the container that opens up when you click the modal button
-            ariaLabelledBy: 'modal-header',
-            templateUrl: 'templates/modal.html',
-            controller: 'CreateRoomCtrl',
-            controllerAs: '$ctrl'
-        });
+    this.open = function() {
+    var modalInstance = $uibModal.open({  //uib modal creates a object on to which you can add functionality.  Imagine the uib modal as the container that opens up when you click the modal button
+        ariaLabelledBy: 'modal-header',
+        templateUrl: 'templates/modal.html',
+        controller: 'CreateRoomCtrl',
+        controllerAs: '$ctrl'
+    });
      
-    }; 
+  }; 
+     
+    this.storeRoomName = function(room) {    //store a room from this.rooms in as scope value
+        this.room = room;
+        this.messages = Message.getByRoomId(room.$id);
+    }
         
   }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);  //do we need ui.boostrap here
+        .controller('HomeCtrl', ['Room', '$uibModal', 'Message', HomeCtrl]);  //do we need ui.boostrap here
 })();
 
