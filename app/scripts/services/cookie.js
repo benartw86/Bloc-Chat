e554a5c4faef66520cabac21c0dded3b4acaf16e
Ben White <masinfa@gmail.com>
2017-02-16 17:39:43 -0500

    Cookies service functional, add user modal functional!

--- app/scripts/services/cookie.js
+++ app/scripts/services/cookie.js
@@ -1,17 +1,27 @@
 (function() {
   function BlocChatCookies($cookies, $uibModal) {
     var currentUser = $cookies.get('blocChatCurrentUser');
-    if (!currentUser || currentUser === '') {
-        $uibModal.open ({
+      if (!currentUser || currentUser === '') {
+        var modalInstance = $uibModal.open ({
           ariaLabelledBy: 'modal-title',
           templateUrl: 'templates/setusermodal.html',
           controller: 'SetUsernameCtrl',
-          controllerAs: '$ctrl'  
-        })
+          controllerAs: '$ctrl',  
+          backdrop: 'static',
+          keyboard: false    
+        })   
+      }
     }
-  }
 
   angular
     .module('blocChat')
     .run(['$cookies', '$uibModal', BlocChatCookies]);
 })();
+
+
+        /* modalInstance.result.then(function(username) {
+        this.username = username;
+        $cookies.put('blocChatCurrentUser', this.username);
+        var currentUser = $cookies.get('blocChatCurrentUser');
+        console.log(currentUser);
+      }); */
--- dist/scripts/controllers/SetUsernameCtrl.js
+++ dist/scripts/controllers/SetUsernameCtrl.js
@@ -1,13 +1,17 @@
 (function() {
     function SetUsernameCtrl($cookies, $uibModalInstance) { //need to inject cookie service?
         this.setUsername = function(newUser) {  //function to add user to cookie
-            $cookies.put('blocChatCurrentUser', 'newUser');  //replace current key with new one
-        }   
+            $cookies.put('blocChatCurrentUser', newUser);  //replace current key with new one
+            if ($cookies.get('blocChatCurrentUser')) {
+                alert('Welcome!  Now click done! ' + newUser);   
+                } else {
+                alert('Enter a username!');
+                }   
+            }
         
         this.ok = function() {
-          var currentUser = $cookies.get('newUser');  
-          if (currentUser) {  //if there is a current user, allow dismissal of modal
-              $uibModalInstance.dismiss('cancel');
+          if ($cookies.get('blocChatCurrentUser')) {  //if there is a current user, allow dismissal of modal
+              $uibModalInstance.dismiss('Done');
         }    
       }
     }
